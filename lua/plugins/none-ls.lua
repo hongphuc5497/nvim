5686849d661e5847c92eaa29a9db7e72fec8ddcf
Hong Phuc <hongphuc.dthp@gmail.com>
2024-01-02 01:04:49 +0700

    nvim: autocompletion & snippets

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,10 +5,13 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint_d,
+
         null_ls.builtins.diagnostics.rubocop,
         null_ls.builtins.formatting.rubocop,
+
+        null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.prettier,
+
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
       },
