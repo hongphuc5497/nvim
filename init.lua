bb54c0143c49d638d796ce93b7b3d28a72c3dc4e
Hong Phuc <hongphuc.dthp@gmail.com>
2024-01-01 22:34:36 +0700

    build: modularize plugins & vim-options

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -11,19 +5,11 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("vim-options")
 require("lazy").setup("plugins")
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {}) 
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal left<CR>', {})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
