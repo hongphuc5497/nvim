5b58a657aae1347eb38592cbcd1c3a512f9ae519
Hong Phuc <hongphuc.dthp@gmail.com>
2024-02-03 19:50:30 +0700

    update teletescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,23 +1,26 @@
 return {
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup({
-				["ui-select"] = {
-					require("telescope.themes").get_dropdown({}),
-				},
-			})
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown({}),
+        },
+        defaults = {
+          file_ignore_patterns = { "node_modules" },
+        },
+      })
 
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
